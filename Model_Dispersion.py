e4f317443af34cf607fe7b95a361d7bd61d5b72c
tbmcoding <tbmcoding@gmail.com>
2018-01-25 22:15:13 -0330

    Added comments with use case example

--- Model_Dispersion.py
+++ Model_Dispersion.py
@@ -4,6 +4,30 @@
 Created on Sun Oct 29 13:06:32 2017
 
 @author: thomas
+
+This file sets up instances of Model_Class_2 with data sets from Model_Data
+
+Model Class was an older version of the implementation. Model_Class_2 is operational but
+still in need of refinement. Once this file is run instnces will be ready to generate dispersion curves.
+
+Use Case
+
+first set up search range
+
+linspace sets search range of frequency ie. linspace(start,stop,number of data points)
+arange sets search range for velocity values ie. arange(low velocity, high velocity,step size)
+  
+omega = linspace(0,100,num=75)
+c = arange(1500,3600,7)
+
+Then we call Create_Disp method to plot our dispersion curves and get dispersion data
+
+w,e,r,t,y,u = Iso2.Create_Disp(c,omega)
+
+the first two values returned are the velocity and frequency values, the others are iterations of determinant
+values mostly for debugging purposes.
+
+Good search ranges are model specific. I will try adding more search examples in the near future. 
 """
 import Model_Data
 import Model_Class
